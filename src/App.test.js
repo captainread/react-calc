b4978030b33d173c1a2bda691d04f37876d99ab9
Alice Read <aread773@gmail.com>
2023-01-24 14:14:38 +0000

    test suite check

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import App from './App';
-
-test("renders calculator", () => {
-  render(<App />);
-  const calculatorElement = screen.getByText(/calculator/i);
-  expect(calculatorElement).toBeInTheDocument();
-});
